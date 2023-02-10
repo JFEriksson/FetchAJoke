224a2e4ab6352bc8713a71b1bdd8213b489a4091
Fredrik Eriksson <110536214+JFEriksson@users.noreply.github.com>
2023-02-10 07:58:06 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+
+const joke = document.querySelector('.dadJoke');
+const button = document.querySelector('.dadJoke')
+const randomColor = document.querySelector('.container')
+const bodyCol = document.querySelector('.body')
+
+function createJoke() {
+fetch('https://icanhazdadjoke.com/', {headers: { 'Accept': 'application/json' }})
+.then(rawResponse => rawResponse.json())
+.then(jsonData => joke.innerHTML = jsonData.joke)
+}
+createJoke()
+
+button.addEventListener('click', function () {
+    let x = Math.floor(Math.random() * 256);
+    let y = Math.floor(Math.random() * 256);
+    let z = Math.floor(Math.random() * 256);
+    let bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    console.log(bgColor);
+    randomColor.style.backgroundColor = bgColor;
+
+    function bodyColor(){
+        let x = Math.floor(Math.random() * 256);
+        let y = Math.floor(Math.random() * 256);
+        let z = Math.floor(Math.random() * 256);
+        let bgColor2 = "rgb(" + x + "," + y + "," + z + ")";
+        console.log(bgColor2);
+        bodyCol.style.backgroundColor = bgColor2;
+    }
+    bodyColor()
+    createJoke()
+})    
